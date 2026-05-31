71c8f08d3b6ecb8c1f10d152ff728e595b23f4d7
dharshianie <40156252+dharshianie@users.noreply.github.com>
2026-05-30 20:00:26 -0500

    test1

--- factorial.py
+++ factorial.py
@@ -1,11 +1,15 @@
+import math
+import os
+
 num = int(input('enter number:' ))
 fac = 1
 for i in list(range (1, num + 1)):
     fac = fac * i
 
 print("the value of", num, "factorial is", fac)
+print("status")
 
 if (fac%2==0):
-    print ("factorial is even number")
+    print ("factorial is an even number")
 else:
-    print ("factorial is odd number")
+    print ("factorial is an odd number")
